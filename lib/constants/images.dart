84b9c030c518270ee24f7e7314ae0bd551e1de59
abdelhamiddsalah <abdelhamidsalah222@gmail.com>
2025-04-01 11:31:30 +0200

    add images

---
+++ lib/constants/images.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesOne
+  /// assets/images/one.png
+  static const String imagesOne = "assets/images/one.png";
+}
+
