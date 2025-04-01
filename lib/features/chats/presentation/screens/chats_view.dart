6c5df7c243b6d0d06e2c1752a7ad7b710a8a015f
abdelhamiddsalah <abdelhamidsalah222@gmail.com>
2025-04-01 11:15:59 +0200

    add packages and chats feature

---
+++ lib/features/chats/presentation/screens/chats_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:messangerapp/features/chats/presentation/widgets/chats_view_body.dart';
+
+class ChatsView extends StatelessWidget {
+  const ChatsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const ChatsViewBody();
+  }
+}
